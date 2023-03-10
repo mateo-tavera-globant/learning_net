ae8078cb170ad45518daff49a3bc326ed218c34f
Mateo Tavera <mateo.tavera@globant.com>
2023-03-10 17:37:03 -0500

    add hello

---
+++ helloWorld/Program.cs
@@ -0,0 +1,7 @@
+﻿using Humanizer;
+
+
+// See https://aka.ms/new-console-template for more information
+Console.Write("Insert your name, please: ");
+var name = Console.ReadLine();
+Console.WriteLine($"Hello, {name.Pascalize()}!");
